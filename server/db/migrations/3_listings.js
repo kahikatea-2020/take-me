93d71beb23042e9fc1a557994af3f2b56b11ed76
Pat-Lim <impatlim@gmail.com>
2020-05-07 11:40:50 +1200

    fist commit

---
+++ server/db/migrations/3_listings.js
@@ -0,0 +1,11 @@
+exports.up = knex =>
+knex.schema.createTable('listings', (table) => {
+  table.increments('id').primary()
+  table.string('name')
+  table.string('description')
+  table.binary('image')
+  table.integer('user_id').references('users.id')
+  table.integer('category_id').references('categories.id')
+})
+
+exports.down = knex => knex.schema.dropTable('listings')
