a39ac95e0f02b1cca607017579acc231169a955c
Mathias Bast <mathiasbast123@gmail.com>
2020-05-07 11:40:14 +1200

    sets up listing route in server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,12 @@ const express = require('express')
 
 const server = express()
 
+const listings = require('././routes/listings')
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+
+// Routes
+server.use('api/v1/listings', listings)
 
 module.exports = server
