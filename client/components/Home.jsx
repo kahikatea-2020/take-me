c0d88b9fe4c763a58307de5de1cb8f84d51cb353
hamishakl <hamish.tana24@gmail.com>
2020-05-07 11:26:01 +1200

    started working on homepage component

---
+++ client/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from 'Navbar'
+import Footer from 'Footer'
+import CategoryList from 'CategoryList'
+
+class Home extends React.Component {
+  render() {
+    <Navbar />
+    return <h1>Homepage</h1>;
+    <Footer />
+  }
+}
+
+export default Home 
