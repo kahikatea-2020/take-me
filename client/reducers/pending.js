f0f7c3859656d9ee47b3b6ca28f714b66606e8da
Lache Melvin <lache.melvin@gmail.com>
2020-05-07 11:43:24 +1200

    fix linting

--- client/reducers/pending.js
+++ client/reducers/pending.js
@@ -7,10 +7,10 @@ export default function pendingReducer (pendingState = false, action) {
   switch (action.type) {
     case GET_LISTINGS_PENDING:
       return true
-    
+
     case GET_LISTINGS_SUCCESS:
       return false
-    
+
     default:
       return pendingState
   }
