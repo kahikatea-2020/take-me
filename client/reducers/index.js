5d33fdbea037248631513cace598a0fe6897177d
Lache Melvin <lache.melvin@gmail.com>
2020-05-07 11:29:00 +1200

    create reducers from home page

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
 import pending from './pending'
+import listings from './listings'
 
 export default combineReducers({
-  pending
+  pending,
+  listings
 })
