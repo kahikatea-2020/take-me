60d3ff8bce42ecfaa12eefacb50281a47d0cb09e
Mathias Bast <mathiasbast123@gmail.com>
2020-05-07 11:55:20 +1200

    fix linting in db folder

--- server/db/migrations/3_listings.js
+++ server/db/migrations/3_listings.js
@@ -1,12 +1,12 @@
 exports.up = knex =>
-knex.schema.createTable('listings', (table) => {
-  table.increments('id').primary()
-  table.string('name')
-  table.string('description')
-  table.string('location')
-  table.binary('image')
-  table.integer('user_id').references('users.id')
-  table.integer('category_id').references('categories.id')
-})
+  knex.schema.createTable('listings', (table) => {
+    table.increments('id').primary()
+    table.string('name')
+    table.string('description')
+    table.string('location')
+    table.binary('image')
+    table.integer('user_id').references('users.id')
+    table.integer('category_id').references('categories.id')
+  })
 
 exports.down = knex => knex.schema.dropTable('listings')
