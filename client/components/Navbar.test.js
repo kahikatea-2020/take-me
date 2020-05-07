c77fe2621ef7c9b0e90d04d291c66e7289f62f6b
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-07 13:45:26 +1200

    Complete second passing test

--- client/components/Navbar.test.js
+++ client/components/Navbar.test.js
@@ -8,3 +8,10 @@ test('Navbar displays two buttons (login/sign up) if user is not logged in', () 
 
   expect(button.length).toBe(2)
 })
+
+test('Navbar displays one button if user is logged in', () => {
+  const wrapper = shallow(<Navbar loggedIn={true} />)
+  const button = wrapper.find('button')
+
+  expect(button.length).toBe(1)
+})
