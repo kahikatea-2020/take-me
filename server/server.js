f940a2a1b25f2ea859a97d185db5aa50aa748d8e
Mathias Bast <mathiasbast123@gmail.com>
2020-05-07 16:07:53 +1200

    adds auth routes to server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const server = express()
 const categories = require('./routes/categories')
 const listings = require('././routes/listings')
+const authRoutes = require('./routes/auth')
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
@@ -11,6 +12,6 @@ server.use(express.static(path.join(__dirname, './public')))
 // Routes
 server.use('/api/v1/listings', listings)
 server.use('/api/v1/categories', categories)
-server.use('/api/v1/user')
+server.use('/api/v1/user', authRoutes)
 
 module.exports = server
