c0caf7aa0249012697b0d418a0276b145de32eb7
Pat-Lim <impatlim@gmail.com>
2020-05-07 16:22:36 +1200

    added tests for getListingsById

--- server/db/listing.test.js
+++ server/db/listing.test.js
@@ -1,5 +1,5 @@
-const env = require('../../tests/test-enviroment')
-const db = require('./listing')
+const env = require("../../tests/test-enviroment")
+const db = require("./listing")
 
 let testDb = null
 
@@ -10,32 +10,49 @@ beforeEach(() => {
 
 afterEach(() => env.cleanup(testDb))
 
-test('test getListings function return test database', () => {
+test("test getListings function return test database", () => {
   const expected = 2
 
-  return db.getListings(testDb)
-    .then(listings => {
-      const actual = listings.length
-      expect(actual).toBe(expected)
-    })
+  return db.getListings(testDb).then((listings) => {
+    const actual = listings.length
+    expect(actual).toBe(expected)
+  })
 })
 
-test('test getListings function return with the name we want', () => {
-  const expected = 'Soccer Ball'
+test("test getListings function return with the name we want", () => {
+  const expected = "Soccer Ball"
 
-  return db.getListings(testDb)
-    .then(listings => {
-      const actual = listings[0].name
-      expect(actual).toBe(expected)
-    })
+  return db.getListings(testDb).then((listings) => {
+    const actual = listings[0].name
+    expect(actual).toBe(expected)
+  })
 })
 
-test('test getListingsById function return with the correct user email', () => {
-  const expected = 'john@gmail.com'
+describe("getListingById tests", () => {
+  test("test getListingsById function return with the correct user email", () => {
+    const expected = "john@gmail.com"
 
-  return db.getListingsById(1, testDb)
-    .then(listing => {
-      const actual = listing.userEmail
+    return db.getListingsById(1, testDb).then((listing) => {
+      const actual = listing[0].userEmail
       expect(actual).toBe(expected)
     })
+  })
+
+  test("test getListingsById function return with the correct user email", () => {
+    const expected = "Mathias"
+
+    return db.getListingsById(2, testDb).then((listing) => {
+      const actual = listing[0].userFirstName
+      expect(actual).toBe(expected)
+    })
+  })
+
+  test("test getListingsById function return with the correct user listing name", () => {
+    const expected = "Ladder"
+
+    return db.getListingsById(2, testDb).then((listing) => {
+      const actual = listing[0].name
+      expect(actual).toBe(expected)
+    })
+  })
 })
