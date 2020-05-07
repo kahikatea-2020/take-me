9b6a6c6ff319e73a1a3d001fe1d39d8eb7aeb388
Mathias Bast <mathiasbast123@gmail.com>
2020-05-07 16:02:20 +1200

    add route for auth

---
+++ server/routes/auth.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { applyAuthRoutes } = require('authenticare/server')
+
+const {
+  userExists,
+  createUser,
+  getUserById } = require('../db/users')
+
+const router = express.Router()
+
+applyAuthRoutes(router, {
+  userExists,
+  createUser,
+  getUserById
+})
+
+module.exports = router
