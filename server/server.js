c22ea06346a2a5dd12b5af63a5d3cf77acdb663b
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-07 15:04:09 +1200

    Add server.get route to fix BrowserRouter cannot GET bug

--- server/server.js
+++ server/server.js
@@ -5,5 +5,8 @@ const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 module.exports = server
