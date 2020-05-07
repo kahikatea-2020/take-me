7b98f4f550c1cb684bb7a54c46c4ba39352065db
Pat-Lim <impatlim@gmail.com>
2020-05-07 14:31:12 +1200

    Add getListingsByID route and function

--- server/db/listing.js
+++ server/db/listing.js
@@ -4,6 +4,18 @@ function getListings (db = connection) {
   return db('listings')
 }
 
+function getListingsById (id, db = connection) {
+  return db('listings')
+    .where('id', id)
+    .select('listings.id as id',
+    'listings.name as name',
+    'listings.description as description',
+    'listings.image_url as imageUrl',
+    'listings.user_id as userId',
+    'listings.category_id as categoryId')
+}
+
 module.exports = {
-  getListings
+  getListings,
+  getListingsById
 }
