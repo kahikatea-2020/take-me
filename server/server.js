5de77f3ee6797eb789552510281d8afca3b23510
john-sengson <j10i_freedom@hotmail.com>
2020-05-07 13:49:10 +1200

    created route for getAllCategory function to return all categories

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const path = require('path')
 const express = require('express')
 
 const server = express()
+const categories = require('./routes/categories')
+
+
+server.use('/api/v1/categories', categories)
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
