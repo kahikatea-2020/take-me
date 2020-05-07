99d462c200fdf28fab1b4df8adb0994fb85655b1
Mathias Bast <mathiasbast123@gmail.com>
2020-05-07 14:43:22 +1200

    update listing db func

--- server/db/listing.js
+++ server/db/listing.js
@@ -2,7 +2,7 @@ const connection = require('./connection')
 
 function getListings (db = connection) {
   return db('listings')
-    .select('listings.id as id', 'listings.name as name', 'listings.description as description', 'listings.image_url as imageUrl', 'listings.user_id as userId', 'listings.category_id as categoryId')
+    .select('listings.id as id', 'listings.name as name', 'listings.image_url as imageUrl', 'listings.user_id as userId', 'listings.category_id as categoryId')
 }
 
 module.exports = {
