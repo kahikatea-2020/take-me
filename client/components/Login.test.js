191a232e7bdb8a940c77054320d27e54083e0518
Mathias Bast <mathiasbast123@gmail.com>
2020-05-08 17:29:43 +1200

    test for log in not working :/

--- client/components/Login.test.js
+++ client/components/Login.test.js
@@ -3,3 +3,9 @@ import renderer from 'react-test-renderer'
 
 import Login from './Login'
 
+test('Test Login component so it renders right', () => {
+  const rendered = renderer
+    .create(<Login />)
+    .toJSON()
+  expect(rendered).toMatchSnapshot()
+})
