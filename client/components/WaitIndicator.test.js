3a4c440a0e237595e893446b5ef1d28e5d9bce21
Mathias Bast <mathiasbast123@gmail.com>
2020-05-09 11:17:28 +1200

    wait indicator working test

--- client/components/WaitIndicator.test.js
+++ client/components/WaitIndicator.test.js
@@ -1,2 +1,12 @@
 import React from 'react'
-import renderWithRedux from '../test-utils'
+import { renderWithRedux } from '../test-utils'
+
+import WaitIndicator from './WaitIndicator'
+
+test('Test the WaitIndicator', () => {
+  const { getByTestId } = renderWithRedux(<WaitIndicator />, {
+    initialState: { pending: true }
+  })
+
+  expect(getByTestId('wait-indicator')).toMatchSnapshot()
+})
