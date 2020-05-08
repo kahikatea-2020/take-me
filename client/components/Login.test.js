f12d1ce4612c8b9636f173e1ba1b13add1c07d1d
Pat-Lim <impatlim@gmail.com>
2020-05-08 18:11:28 +1200

    added testing library and fixed login snapshot test

--- client/components/Login.test.js
+++ client/components/Login.test.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import { renderWithRedux } from '../test-utils'
+import '@testing-library/jest-dom/extend-expect'
 
 import Login from './Login'
 
 test('Test Login component so it renders right', () => {
-  const rendered = renderer
-    .create(<Login />)
-    .toJSON()
+  const rendered = renderWithRedux(<Login />)
   expect(rendered).toMatchSnapshot()
 })
