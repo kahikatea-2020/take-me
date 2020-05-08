a7e07149acb3b74e1cda56561aa1f5a75cfa2136
Mathias Bast <mathiasbast123@gmail.com>
2020-05-09 09:46:50 +1200

    add wait indicator gif

--- client/components/WaitIndicator.jsx
+++ client/components/WaitIndicator.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const WaitIndicator (props) => {
+const WaitIndicator = (props) => {
   return props.pending
-    ? <img data-testid='wait-indicator'
+    ? <img data-testid='wait-indicator' src='/loadIndicator.gif' alt='loading indicator' style={{width: 50 }} />
+    : null
 }
