935cd2a34c5c62788561250c9cee7745df02667d
hamishakl <hamish.tana24@gmail.com>
2020-05-09 10:45:29 +1200

    added semantic to homepage lsiting cards

--- client/components/ListItem.jsx
+++ client/components/ListItem.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 class ListItem extends React.Component {
   render () {
     const listing = this.props.listing
     return (
       <>
-        <img style={{maxWidth: '200px'}} src={`https://res.cloudinary.com/takemenz/image/upload/${JSON.parse(listing.imageUrl)[0]}`} alt={listing.name}/>
-        <h3><Link to={`/listings/${listing.id}`}>{listing.name}</Link></h3>
+        <div class="ui card">
+          <a class="image" href={`/listings/${listing.id}`}>
+            <img style={{maxWidth: '200px'}} src={`https://res.cloudinary.com/takemenz/image/upload/${JSON.parse(listing.imageUrl)[0]}`} alt={listing.name}/>
+          </a>
+          <div class="content">
+            <h3><Link to={`/listings/${listing.id}`}>{listing.name}</Link></h3>
+            <div class="meta">
+              <a>{listing.location}</a>
+            </div>
+          </div>
+        </div>
       </>
     )
   }
--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -33,6 +33,7 @@ class Profile extends React.Component {
           </div>
         </div>
         
+        
       </>
     )
   }
