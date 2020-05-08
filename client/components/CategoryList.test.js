7612606d157070abd50c5c8f364b4fb52510031c
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-09 11:32:43 +1200

    Refactor test to mock - fix bug

--- client/components/CategoryList.test.js
+++ client/components/CategoryList.test.js
@@ -1,20 +1,26 @@
 import React from 'react'
-import { screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+// import { screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import { renderWithRedux } from '../test-utils'
 
 import CategoryList from './CategoryList'
 
-const categories = [
-  { id: 1, name: 'Clothing' },
-  { id: 2, name: 'Electronics' },
-  { id: 3, name: 'Furniture' }
-]
+jest.mock('../api/categories', () => {
+  return {
+    getCategories: () =>
+      Promise.resolve([
+        { id: 1, name: 'Clothing' },
+        { id: 2, name: 'Electronics' },
+        { id: 3, name: 'Furniture' }
+      ])
+  }
+})
 
 test('<CategoryList> renders a dropdown menu with categories from the store', () => {
-  renderWithRedux(<CategoryList />, { initialState: { categories } })
-  screen.findAllByRole('option').then((options) => {
-    expect(options.length).toBe(categories.length)
-    expect(options[2]).toHaveTextContent('Furniture')
-  })
+  const { findAllByRole } = renderWithRedux(<CategoryList />)
+  findAllByRole('option')
+    .then(options => {
+      expect(options.length).toBe(3)
+      expect(options[2]).toHaveTextContent('Furniture')
+    })
 })
