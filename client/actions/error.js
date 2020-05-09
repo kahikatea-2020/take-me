dc104af3be5f9d261801b03fc298eaa1c28b092f
Mathias Bast <mathiasbast123@gmail.com>
2020-05-09 18:43:14 +1200

    adds to error handler

--- client/actions/error.js
+++ client/actions/error.js
@@ -1,8 +1,15 @@
 export const ERROR = 'ERROR'
+export const HIDE_ERROR = 'HIDE_ERROR'
 
 export function showError (message) {
   return {
     type: ERROR,
     message
+  }
+}
+
+export function hideError () {
+  return {
+    type: HIDE_ERROR
   }
 }
