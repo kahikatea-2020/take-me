00beee704088978d8f5c1a93b2fcedd738c4cb81
Mathias Bast <mathiasbast123@gmail.com>
2020-05-09 18:44:20 +1200

    adds error reducer to hide;

--- client/reducers/error.js
+++ client/reducers/error.js
@@ -1,9 +1,12 @@
-import { ERROR } from '../actions/error'
+import { ERROR, HIDE_ERROR } from '../actions/error'
 
 export default function (state = null, action) {
   switch (action.type) {
     case ERROR:
       return action.message
+
+    case HIDE_ERROR:
+      return state
 
     default:
       return state
