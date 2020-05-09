66ad2a9ea12d659c4ce52158b3532af18b5ba68d
hamishakl <hamish.tana24@gmail.com>
2020-05-10 10:37:04 +1200

    added in input fields

--- client/components/updateListing.jsx
+++ client/components/updateListing.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Form } from 'semantic-ui-react'
 
 class updateListing extends React.Component {
   render () {
     return (
       <>
-        <p>hello </p>
+        <h1>Update Listing</h1>
+        <Form>
+          <div className="ui form">
+            <div className="field">
+              <label>Listing Name</label>
+              <input type="text"/>
+            </div>
+            <div className="field">
+              <label>Description</label>
+              <textarea></textarea>
+            </div>
+            <div className="field">
+              <label></label>
+              <input type="text"/>
+            </div>
+          </div>
+        </Form>
       </>
     )
   }
