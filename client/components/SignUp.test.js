9a5b896cc5a87e9b84d2a4c858974ab515042754
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-09 15:45:33 +1200

    Attempt third test - not working

--- client/components/SignUp.test.js
+++ client/components/SignUp.test.js
@@ -16,3 +16,14 @@ test('<SignUp /> has the correct number of inputs', () => {
   const wrapper = shallow(<SignUp />)
   expect(wrapper.find(Form.Input).length).toBe(8)
 })
+
+// This test isn't finished and isn't passing, help
+xtest('State gets updated when input fields are filled in', () => {
+  const mock = jest.fn()
+  const wrapper = shallow(<SignUp onKeyUp={mock}/>)
+  const firstNameInput = wrapper.find(Form.Input).at(0)
+  firstNameInput.simulate('keyUp', {
+    currentTarget: { name: 'firstName', value: 'ellora' }
+  })
+  expect(mock).toHaveBeenCalledWith('ellora')
+})
