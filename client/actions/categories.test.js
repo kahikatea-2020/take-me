f48939d5b1de8b4643485a87acd10ea7da26ca38
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-10 09:59:52 +1200

    Write passing test for getCategoriesSuccess

--- client/actions/categories.test.js
+++ client/actions/categories.test.js
@@ -16,3 +16,26 @@ test('selectedCategoryChange gets category successfully', () => {
   expect(action.category.name).toMatch('Bob')
   expect(action.type).toBe(SELECTED_CATEGORY_CHANGE)
 })
+
+test('getCategories has getCategoriesSuccess function that dispatches success', () => {
+  const categories = [
+    {
+      id: 1,
+      name: 'Trucks'
+    },
+    {
+      id: 300,
+      name: 'Dolls'
+    },
+    {
+      id: 96,
+      name: 'Popsicles'
+    }
+  ]
+  const action = getCategoriesSuccess(categories)
+
+  expect(action.type).toBe(GET_CATEGORIES_SUCCESS)
+  expect(action.categories[1].id).toBe(300)
+  expect(action.categories[2].name).toMatch('Popsicles')
+  expect(action.categories).toHaveLength(3)
+})
