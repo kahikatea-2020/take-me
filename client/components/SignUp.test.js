ec4b003307d15b99668d962fd2ad752ae8b7324f
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-09 14:59:51 +1200

    Add second passing test - correct number of inputs

--- client/components/SignUp.test.js
+++ client/components/SignUp.test.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { shallow } from 'enzyme'
+import { Form } from 'semantic-ui-react'
+
 import { renderWithRedux } from '../test-utils'
 import '@testing-library/jest-dom'
 
 import SignUp from './SignUp'
 
-test('Sign up component renders correctly', () => {
+test('<SignUp /> renders correctly', () => {
   const rendered = renderWithRedux(<SignUp />)
   expect(rendered).toMatchSnapshot()
+})
+
+test('<SignUp /> has the correct number of inputs', () => {
+  const wrapper = shallow(<SignUp />)
+  expect(wrapper.find(Form.Input).length).toBe(8)
 })
