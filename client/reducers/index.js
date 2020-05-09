6fab55f118203a45e1007cfaa40d36f036524e07
Lache Melvin <lache.melvin@gmail.com>
2020-05-09 14:28:48 +1200

    refer to logged in user as user in state

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,12 +4,12 @@ import pending from './pending'
 import listings from './listings'
 import categories from './categories'
 import selectedCategory from './selectedCategory'
-import users from './users'
+import user from './users'
 
 export default combineReducers({
   pending,
   listings,
   categories,
   selectedCategory,
-  users
+  user
 })
