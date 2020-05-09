b90d2151c6a15b11033dede14a8f87dcfaeb07d7
Lache Melvin <lache.melvin@gmail.com>
2020-05-09 20:41:45 +1200

    get user details with toek non page load

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { CloudinaryContext } from 'cloudinary-react'
+import { connect } from 'react-redux'
+import { isAuthenticated } from 'authenticare/client'
+
 
 import Navbar from './Navbar'
 import SignUp from './SignUp'
@@ -9,25 +12,34 @@ import Home from './Home'
 import Listing from './Listing'
 import NewListing from './NewListing'
 import Profile from './Profile'
+import { getUserDetails } from '../actions/users'
 // import Footer from './Footer'
 
-const App = () => {
-  return (
-    <CloudinaryContext cloudName='takemenz'>
-      <Router>
-        <Route path='/' component={Navbar} />
-        <Route exact path='/' component={Home} />
-        <Route path='/sign-up' component={SignUp} />
-        <Route path='/login' component={Login} />
-        <Route path='/new-listing' component={NewListing} />
-        <Route exact path='/listings/:id' component={Listing} />
-        <Route path='/profile/:id' component={Profile} />
-        {/* <Route path='/' component={Footer} /> */}
-      </Router>
-    </CloudinaryContext>
-  )
+class App extends React.Component {
+  componentDidMount () {
+    if (isAuthenticated()) {
+      this.props.dispatch(getUserDetails())
+    }
+  }
+
+  render () {
+    return (
+      <CloudinaryContext cloudName='takemenz'>
+        <Router>
+          <Route path='/' component={Navbar} />
+          <Route exact path='/' component={Home} />
+          <Route path='/sign-up' component={SignUp} />
+          <Route path='/login' component={Login} />
+          <Route path='/new-listing' component={NewListing} />
+          <Route exact path='/listings/:id' component={Listing} />
+          <Route path='/profile/:id' component={Profile} />
+          {/* <Route path='/' component={Footer} /> */}
+        </Router>
+      </CloudinaryContext>
+    )
+  }
 }
 
 //
 
-export default App
+export default connect()(App)
