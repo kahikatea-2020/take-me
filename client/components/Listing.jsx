9d526e951f43e35bfc0d4a208c97d7fca4cd7e15
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-10 15:03:16 +1200

    Get carousel working with data from database

--- client/components/Listing.jsx
+++ client/components/Listing.jsx
@@ -15,53 +15,46 @@ class Listing extends React.Component {
       .then(listing => {
         if (listing === undefined) {
           this.props.history.push(`/404`)
+        } else {
+          this.setState({
+            listing,
+            emailSubject: listing.name.split(' ').join('%20'),
+            description: listing.description,
+            imageUrl: listing.imageUrl
+          })
         }
-        this.setState({
-          listing,
-          emailSubject: listing.name.split(' ').join('%20'),
-          description: listing.description,
-          imageUrl: listing.imageUrl
-        })
       })
   }
   render() {
     const { listing } = this.state
-
     const settings = {
       dots: true,
       infinite: true,
       fade: true,
-      adaptiveHeight: true,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
       className: 'slides'
     }
 
-    const imgUrls = [
-      'https://i.ytimg.com/vi/tVlcKp3bWH8/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/kJ2dr9jAThY/maxresdefault.jpg',
-      'https://i.pinimg.com/originals/dd/59/4e/dd594e241abf617abed2b7d586c19ef9.jpg'
-    ]
-
     return (
       <>
         <div className="listingWrapper">
           <h2>{listing.name}</h2>
-          <div className='slick-carousel'>
-            <Slider className='slick-image' {...settings}>
-              {/* {this.state.imageUrl.map((url, idx) => { */}
-              {imgUrls.map((url, idx) =>
-                <img
-                  key={idx}
-                  className='slick-image'
-                  // src={`https://res.cloudinary.com/takemenz/image/upload/${url}`}
-                  src={`${url}`}
-                  alt={listing.name}
-                />
-              )}
-            </Slider>
-          </div>
+          {(this.state.imageUrl[0] !== undefined ) &&
+            <div className='slick-carousel'>
+              <Slider className='slick-image' {...settings}>
+                {this.state.imageUrl.map((url, idx) => (
+                  <img
+                    key={idx}
+                    className='slick-image'
+                    src={`https://res.cloudinary.com/takemenz/image/upload/${url}`}
+                    alt={listing.name}
+                  />
+                ))}
+              </Slider>
+            </div>
+          }
           <div className='listing-description'>
             {this.state.description.map(sentence => <p key={sentence.substr(0, 10)}>{sentence}</p>)}
           </div>
