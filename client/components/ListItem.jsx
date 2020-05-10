9e61a1cd28088fce7f52e5471a27627b12ca2fe2
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-10 18:55:15 +1200

    Fix link issue etc.

--- client/components/ListItem.jsx
+++ client/components/ListItem.jsx
@@ -8,19 +8,20 @@ class ListItem extends React.Component {
     return (
       <Card>
         <Link to={`/listings/${listing.id}`}>
-            <Image centered={true} style={{ height: '300px', width: '100%', objectFit: 'cover'}} src={`https://res.cloudinary.com/takemenz/image/upload/${listing.imageUrl[0]}`} alt={listing.name} />
-            </Link>
+          <Image centered={true} style={{ height: '300px', width: '100%', objectFit: 'cover'}} src={`https://res.cloudinary.com/takemenz/image/upload/${listing.imageUrl[0]}`} alt={listing.name} />
+        </Link>
         <Card.Content>
-          
-            <Card.Header>{listing.name}</Card.Header>
-            <Card.Meta>{listing.location}</Card.Meta>
-            <Card.Description>{listing.category}</Card.Description>
-            {/* <div className="content">
-              <h3><Link to={`/listings/${listing.id}`}>{listing.name}</Link></h3>
-              <div className="meta">
-                <a>{listing.location}</a>
-              </div>
-            </div> */}
+          <Card.Header as={Link} to={`/listings/${listing.id}`}>
+            {listing.name}
+          </Card.Header>
+          <Card.Meta>{listing.location}</Card.Meta>
+          <Card.Description>{listing.category}</Card.Description>
+          {/* <div className="content">
+            <h3><Link to={`/listings/${listing.id}`}>{listing.name}</Link></h3>
+            <div className="meta">
+              <a>{listing.location}</a>
+            </div>
+          </div> */}
         </Card.Content>
       </Card>
     )
