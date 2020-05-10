e76e5ff3cb94be0ed39199b1ee94bca211b93893
Lache Melvin <lache.melvin@gmail.com>
2020-05-11 11:17:55 +1200

    create getUserPending action

--- client/actions/users.js
+++ client/actions/users.js
@@ -4,6 +4,7 @@ import * as api from '../api/users'
 export const GET_USER_SUCCESS = 'GET_USER_SUCCESS'
 export const LOGIN_PENDING = 'LOGIN_PENDING'
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const GET_USER_PENDING = 'GET_USER_PENDING'
 
 export function getUserSuccess (details) {
   return {
@@ -12,8 +13,15 @@ export function getUserSuccess (details) {
   }
 }
 
+export function getUserPending () {
+  return {
+    type: GET_USER_PENDING
+  }
+}
+
 export function getUserDetails () {
   return dispatch => {
+    dispatch(getUserPending())
     return api.getUserDetails()
       .then(details => {
         dispatch(getUserSuccess(details))
