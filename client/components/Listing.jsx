f8458b2b785fbb9a33a9d5950329f3b165d88463
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-10 12:13:26 +1200

    React Slick carousel works with demo images

--- client/components/Listing.jsx
+++ client/components/Listing.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Slider from 'react-slick'
 
 import { getListingById } from '../api/listings'
 
@@ -9,10 +10,10 @@ class Listing extends React.Component {
     description: [],
     imageUrl: []
   }
-  componentDidMount () {
+  componentDidMount() {
     getListingById(this.props.match.params.id)
       .then(listing => {
-        if(listing === undefined) {
+        if (listing === undefined) {
           this.props.history.push(`/404`)
         }
         this.setState({
@@ -23,28 +24,54 @@ class Listing extends React.Component {
         })
       })
   }
-  render () {
+  render() {
     const { listing } = this.state
+
+    const settings = {
+      dots: true,
+      infinite: true,
+      fade: true,
+      adaptiveHeight: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      className: 'slides'
+    }
+
+    const imgUrls = [
+      'https://i.ytimg.com/vi/tVlcKp3bWH8/maxresdefault.jpg',
+      'https://i.ytimg.com/vi/kJ2dr9jAThY/maxresdefault.jpg',
+      'https://i.pinimg.com/originals/dd/59/4e/dd594e241abf617abed2b7d586c19ef9.jpg'
+    ]
 
     return (
       <>
-      <div className="listingWrapper">
-        <h2>{listing.name}</h2>
-        <img
-          style={{ maxWidth: '400px' }}
-          src={`https://res.cloudinary.com/takemenz/image/upload/${this.state.imageUrl[0]}`}
-          alt={listing.name} 
-        /> {/* currently just using the first picture link in the array, needs to be a carousel :) */}
-        {this.state.description.map(sentence => <p key={sentence.substr(0, 10)}>{sentence}</p>)}
-      </div>
-      <div className='contactInfo'>
-        <h4>Location: {listing.location}</h4>
-        <h3>Contact {listing.userFirstName}</h3>
-        <p>{listing.userPhoneNumber}</p>
-        <button className='emailButton'>
-          <a href={`mailto:${listing.userEmail}?subject=#${listing.id}:%20${this.state.emailSubject}`}>Email Dealer</a>
-        </button>
-      </div>
+        <div className="listingWrapper">
+          <h2>{listing.name}</h2>
+          <div className='slick-carousel'>
+            <Slider className='slick-image' {...settings}>
+              {/* {this.state.imageUrl.map((url, idx) => { */}
+              {imgUrls.map((url, idx) =>
+                <img
+                  key={idx}
+                  className='slick-image'
+                  // src={`https://res.cloudinary.com/takemenz/image/upload/${url}`}
+                  src={`${url}`}
+                  alt={listing.name}
+                />
+              )}
+            </Slider>
+          </div>
+          {this.state.description.map(sentence => <p key={sentence.substr(0, 10)}>{sentence}</p>)}
+        </div>
+        <div className='contactInfo'>
+          <h4>Location: {listing.location}</h4>
+          <h3>Contact {listing.userFirstName}</h3>
+          <p>{listing.userPhoneNumber}</p>
+          <button className='emailButton'>
+            <a href={`mailto:${listing.userEmail}?subject=#${listing.id}:%20${this.state.emailSubject}`}>Email Dealer</a>
+          </button>
+        </div>
       </>
     )
   }
