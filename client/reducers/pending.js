ad0625ad8d8511e264200b5331cfbba1de800ec4
Lache Melvin <lache.melvin@gmail.com>
2020-05-11 11:41:24 +1200

    add get categories to pending reducer

--- client/reducers/pending.js
+++ client/reducers/pending.js
@@ -12,18 +12,25 @@ import {
   GET_USER_SUCCESS
 } from '../actions/users'
 
+import {
+  GET_CATEGORIES_PENDING,
+  GET_CATEGORIES_SUCCESS
+} from '../actions/categories'
+
 export default function pendingReducer (pendingState = false, action) {
   switch (action.type) {
     case LOGIN_PENDING:
     case GET_USERS_LISTINGS_PENDING:
     case GET_LISTINGS_PENDING:
     case GET_USER_PENDING:
+    case GET_CATEGORIES_PENDING:
       return true
 
     case LOGIN_SUCCESS:
     case GET_USERS_LISTINGS_SUCCESS:
     case GET_LISTINGS_SUCCESS:
     case GET_USER_SUCCESS:
+    case GET_CATEGORIES_SUCCESS:
       return false
 
     default:
