70d20ddd5f815e1c75a3690a28a88ae762169801
Lache Melvin <lache.melvin@gmail.com>
2020-05-11 11:26:49 +1200

    add pending tests

--- client/reducers/pending.test.js
+++ client/reducers/pending.test.js
@@ -1,10 +1,15 @@
 import {
   GET_LISTINGS_PENDING,
-  GET_LISTINGS_SUCCESS
+  GET_LISTINGS_SUCCESS,
+  GET_USERS_LISTINGS_SUCCESS,
+  GET_USERS_LISTINGS_PENDING
 } from '../actions/listings'
+
 import {
   LOGIN_PENDING,
-  LOGIN_SUCCESS
+  LOGIN_SUCCESS,
+  GET_USER_PENDING,
+  GET_USER_SUCCESS
 } from '../actions/users'
 import pending from './pending'
 
@@ -36,6 +41,40 @@ describe('Login', () => {
   it('should return false when success action dispatched', () => {
     const action = {
       type: LOGIN_SUCCESS
+    }
+    const newState = pending(true, action)
+    expect(newState).toBeFalsy()
+  })
+})
+
+describe('Get User', () => {
+  it('should return true when pending action dispatched', () => {
+    const action = {
+      type: GET_USER_PENDING
+    }
+    const newState = pending(false, action)
+    expect(newState).toBeTruthy()
+  })
+  it('should return false when success action dispatched', () => {
+    const action = {
+      type: GET_USER_SUCCESS
+    }
+    const newState = pending(true, action)
+    expect(newState).toBeFalsy()
+  })
+})
+
+describe('Ger User Listings', () => {
+  it('should return true when pending action dispatched', () => {
+    const action = {
+      type: GET_USERS_LISTINGS_PENDING
+    }
+    const newState = pending(false, action)
+    expect(newState).toBeTruthy()
+  })
+  it('should return false when success action dispatched', () => {
+    const action = {
+      type: GET_USERS_LISTINGS_SUCCESS
     }
     const newState = pending(true, action)
     expect(newState).toBeFalsy()
