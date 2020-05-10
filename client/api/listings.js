e5f5c8e5b1910f4114b0bf607bca41f7f78eac36
Mathias Bast <mathiasbast123@gmail.com>
2020-05-10 16:21:23 +1200

    add apiplz

---
+++ client/api/listings.js
@@ -0,0 +1,34 @@
+import request from 'superagent'
+import { getEncodedToken } from 'authenticare/client'
+
+export function getListings () {
+  return request.get('/api/v1/listings')
+    .then(res => res.body)
+}
+
+export function getListingById (id) {
+  return request.get(`/api/v1/listings/${id}`)
+    .then(res => res.body[0])
+}
+
+export function addListing (listing) {
+  return request.post('/api/v1/listings/new')
+    .send(listing)
+    .then(res => res.body.id)
+}
+
+export function deleteListingById (id) {
+  return request.delete(`/api/v1/listings/${id}`)
+    .set({ 'Accept': 'application/json' })
+    .set({ 'Authorization': `Bearer ${getEncodedToken()}` })
+    .then(res => res.status)
+    .catch(err => console.log(err.message))
+    
+  }
+
+  export function getUsersListings (id) {
+    return request.get(`/api/v1/listings/user/${id}`)
+      .set({ 'Accept': 'application/json' })
+      .set({ 'Authorization': `Bearer ${getEncodedToken()}` })
+      .then(res => res.body)
+  }
