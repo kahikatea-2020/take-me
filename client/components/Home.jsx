85571774ef35d8dc29f630547cb2f8e18d57602b
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-10 15:51:36 +1200

    Begin styling with semantic ui react

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 
 import WaitIndicator from './WaitIndicator'
 import { getListings } from '../actions/listings'
+import { Card } from 'semantic-ui-react'
 // redux
 
 // import CategoryList from 'CategoryList'
@@ -20,9 +21,9 @@ class Home extends React.Component {
         <h1>Latest Listings</h1>
         {/* <Search /> */}
         <WaitIndicator />
-        <div className="ListingWrapper">
+        <Card.Group>
           {this.props.listings.map(item => <ListItem key={item.id} listing={item} />)}
-        </div>
+        </Card.Group>
       </>
     )
   }
--- client/components/ListItem.jsx
+++ client/components/ListItem.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { Card } from 'semantic-ui-react'
 
 class ListItem extends React.Component {
   render () {
     const listing = this.props.listing
     return (
       <>
-        <div className="ui card">
+        <Card>
           <a className="image" href={`/listings/${listing.id}`}>
             <img style={{maxWidth: '200px'}} src={`https://res.cloudinary.com/takemenz/image/upload/${JSON.parse(listing.imageUrl)[0]}`} alt={listing.name}/>
           </a>
@@ -17,7 +17,7 @@ class ListItem extends React.Component {
               <a>{listing.location}</a>
             </div>
           </div>
-        </div>
+        </Card>
       </>
     )
   }
