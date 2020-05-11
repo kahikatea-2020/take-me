e6c149194b6d38f53efea6f9e65bbf9c4dcc6977
Lache Melvin <lache.melvin@gmail.com>
2020-05-12 11:38:42 +1200

    create get comments db function

---
+++ server/db/comments.js
@@ -0,0 +1,18 @@
+const connection = require('./connection')
+
+function getCommentsById (id, db = connection) {
+  return db('q_and_a')
+    .join('users', 'q_and_a.user', 'users.id')
+    .where('q_and_a.listing_id', id)
+    .select('q_and_a.id as id',
+      'q_and_a.comment as comment',
+      'q_and_a.date as date',
+      'q_and_a.user_id as userId',
+      'users.first_name as userFirstName',
+      'users.image_url as userImage'
+    )
+}
+
+module.exports = {
+  getCommentsById
+}
