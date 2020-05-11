07199faaea2e08932c80da9f6168eec95c2725dc
Mathias Bast <mathiasbast123@gmail.com>
2020-05-11 22:24:47 +1200

    adds more to about

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 class About extends React.Component {
   render () {
     return (
-      <h1>Welcome To Take Me</h1>
-      <h2>What is The Point of This Website?</h2>
-      <p></p>
+      <>
+        <h1>Welcome To Take Me</h1>
+        <h2>What is The Point of This Website?</h2>
+
+      </>
     )
   }
 }
