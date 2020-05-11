278c7d58862afb27d4f979b8cbfe73e086d882b7
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-11 18:54:57 +1200

    Styling on listing page woo

--- client/components/Listing.jsx
+++ client/components/Listing.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Slider from 'react-slick'
 import { isAuthenticated } from 'authenticare/client'
 import { connect } from 'react-redux'
-import { Button, Image, Card, Grid } from 'semantic-ui-react'
+import { Button, Card, Grid, Image } from 'semantic-ui-react'
 
 import WaitIndicator from './WaitIndicator'
 
@@ -23,7 +23,7 @@ class Listing extends React.Component {
       .then(listing => {
         this.props.dispatch(getListingSuccess())
         if (listing === undefined) {
-              this.props.history.push(`/404`)
+          this.props.history.push(`/404`)
         } else {
           this.setState({
             listing,
@@ -51,66 +51,64 @@ class Listing extends React.Component {
     return (
       <Grid columns={2} divided>
         <Grid.Row>
-          <Grid.Column>
-          {(this.state.imageUrl[0] !== undefined ) &&
-            <div className='slick-carousel'>
-              <Slider className='slick-image' {...settings}>
-                {this.state.imageUrl.map((url, idx) => (
-                  <img
-                    key={idx}
-                    className='slick-image'
-                    src={`https://res.cloudinary.com/takemenz/image/upload/${url}`}
-                    alt={listing.name}
-                  />
-                ))}
-              </Slider>
-            </div>
-          }
+          <Grid.Column className='listing-images'>
+            {(this.state.imageUrl[0] !== undefined) &&
+              <div className='slick-carousel'>
+                <Slider className='slick-image' {...settings}>
+                  {this.state.imageUrl.map((url, idx) => (
+                    <img
+                      key={idx}
+                      className='slick-image'
+                      src={`https://res.cloudinary.com/takemenz/image/upload/${url}`}
+                      alt={listing.name}
+                    />
+                  ))}
+                </Slider>
+              </div>
+            }
           </Grid.Column>
-          <Grid.Column>
-          <div className='listing-description'>
-            <h1>{listing.name}</h1>
-            {this.state.description.map(sentence => <p key={sentence.substr(0, 10)}>{sentence}</p>)}
-          </div>
-        <div className='contact-info'>
-          <h4>Location: {listing.location}</h4>
+          <Grid.Column stretched className='listing-details'>
+            <div className='listing-description'>
+              <h1>{listing.name}</h1>
+              <h4>Location: {listing.location}</h4>
+              {this.state.description.map(sentence => <p key={sentence.substr(0, 10)}>{sentence}</p>)}
+            </div>
+            <div className='contact-info'>
               <Card>
                 <Card.Content>
                   <Image
                     circular
                     floated='right'
-                    size='mini'
                     src={`https://res.cloudinary.com/takemenz/image/upload/${listing.userImage}`}
                   />
                   <Card.Header>Contact {listing.userFirstName}</Card.Header>
-                  <Card.Description>
+                  <Card.Meta>
                     Phone: {listing.userPhoneNumber}
-                  </Card.Description>
+                  </Card.Meta>
                 </Card.Content>
                 <Card.Content extra>
-                  <div className='ui two buttons'>
-                    <Button className='email-button'>
-                      <a href={`mailto:${listing.userEmail}?subject=#${listing.id}:%20${this.state.emailSubject}`}>Email</a>
+                  <div className='ui-two-buttons'>
+                    <Button id='email' as={Link} to={`mailto:${listing.userEmail}?subject=#${listing.id}:%20${this.state.emailSubject}`}>
+                      Email
                     </Button>
                     {/* <Link to={`/profile/${listing.userId}`} > */}
-                      <Button as={Link} to={`/profile/${listing.userId}`}>
-                        View Profile
+                    <Button id='profile' as={Link} to={`/profile/${listing.userId}`}>
+                      View Profile
                       </Button>
                     {/* </Link> */}
                   </div>
                 </Card.Content>
               </Card>
-          {(isAuthenticated() && (this.props.user.id === listing.userId)) &&
-            <button className='updateListing'>
-              <Link to={`/update-listing/${listing.id}`}>Edit Listing</Link>
-            </button>
-           }
-        </div>
-          <br />
-          {(isAuthenticated() && (this.props.user.id === listing.userId)) &&
-            <Link to={`/profile/${listing.userId}`} ><button>Your listings</button></Link>
-          }
-        <WaitIndicator />
+            </div>
+            {(isAuthenticated() && (this.props.user.id === listing.userId)) &&
+              <button className='update-listing'>
+                <Link to={`/update-listing/${listing.id}`}>Edit Listing</Link>
+              </button>
+            }
+            {(isAuthenticated() && (this.props.user.id === listing.userId)) &&
+              <Link to={`/profile/${listing.userId}`} ><button>Your listings</button></Link>
+            }
+            <WaitIndicator />
           </Grid.Column>
         </Grid.Row>
       </Grid>
