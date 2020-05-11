91d38f04d29ce65bc0326128f7975189a4a656ac
Mathias Bast <mathiasbast123@gmail.com>
2020-05-11 22:27:38 +1200

    and anoter one

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -19,6 +19,7 @@ class About extends React.Component {
           <li>You are saving energy - because the manufacturer does not need to make a new product</li>
           <li>Reduces landfill use - if we can reduce the amount of waste in a landfill, we will be better off as a planet</li>
           <li>Reduces the number of chemicals used - factories use raw materials which create toxic chemicals</li>
+          <li>Reduces water pollution - if an item is recycled, it will not end up in the ocean</li>
           
         </ul>
 
