67a735f878961d58487334d64ce8f8a35f0c97d2
Lache Melvin <lache.melvin@gmail.com>
2020-05-11 18:36:27 +1200

    use longer string fields for description and image urls

--- server/db/migrations/3_listings.js
+++ server/db/migrations/3_listings.js
@@ -2,8 +2,8 @@ exports.up = knex =>
   knex.schema.createTable('listings', (table) => {
     table.increments('id').primary()
     table.string('name')
-    table.string('description', 2000)
-    table.string('image_url', 1000)
+    table.string('description', 3000)
+    table.string('image_url', 3000)
     table.string('location')
     table.integer('user_id').references('users.id')
     table.integer('category_id').references('categories.id')
