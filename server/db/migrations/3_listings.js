3ccce65bd268db815a974c6bf6bd739c9e9e549c
Lache Melvin <lache.melvin@gmail.com>
2020-05-11 18:39:57 +1200

    trying with text fields instead of string

--- server/db/migrations/3_listings.js
+++ server/db/migrations/3_listings.js
@@ -2,8 +2,8 @@ exports.up = knex =>
   knex.schema.createTable('listings', (table) => {
     table.increments('id').primary()
     table.string('name')
-    table.string('description', 3000)
-    table.string('image_url', 3000)
+    table.text('description', 'longtext')
+    table.text('image_url')
     table.string('location')
     table.integer('user_id').references('users.id')
     table.integer('category_id').references('categories.id')
