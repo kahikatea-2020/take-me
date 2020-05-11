9b5e3317f17d44763dc88ca7bcbb3204d133771a
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-11 15:07:31 +1200

    Fix tests to pass yaayyyyy

--- client/components/SignUp.test.js
+++ client/components/SignUp.test.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import { Form } from 'semantic-ui-react'
 import { applyMiddleware, createStore } from 'redux'
+import { BrowserRouter as Router } from 'react-router-dom'
 import thunkMiddleware from 'redux-thunk'
 import { Provider } from 'react-redux'
 
@@ -10,31 +11,28 @@ import reducers from '../reducers'
 import { renderWithRedux } from '../test-utils'
 import '@testing-library/jest-dom'
 
-import SignUp from './SignUp'
+import SignUp, { VanillaSignUp } from './SignUp'
 
 test('<SignUp /> renders correctly', () => {
   const rendered = renderWithRedux(<SignUp />)
   expect(rendered).toMatchSnapshot()
 })
 
-// this needs fixing
-xtest('<SignUp /> has the correct number of inputs', () => {
-  const store = createStore(
-    reducers,
-    applyMiddleware(thunkMiddleware)
-  )
-  const wrapper = shallow(<Provider store={store}><SignUp /></Provider>)
-  console.log(wrapper)
+test('<SignUp /> has the correct number of inputs', () => {
+  const store = createStore(reducers, applyMiddleware(thunkMiddleware))
+
+  const wrapper = mount(<Provider store={store}><Router><SignUp /></Router></Provider>)
   expect(wrapper.find(Form.Input).length).toBe(8)
 })
 
 // This test isn't finished and isn't passing, help
-xtest('State gets updated when input fields are filled in', () => {
-  const mock = jest.fn()
-  const wrapper = shallow(<SignUp onKeyUp={mock}/>)
-  const firstNameInput = wrapper.find(Form.Input).at(0)
-  firstNameInput.simulate('keyUp', {
-    currentTarget: { name: 'firstName', value: 'ellora' }
-  })
-  expect(mock).toHaveBeenCalledWith('ellora')
+test('State gets updated when input fields are filled in', () => {
+  const store = createStore(reducers, applyMiddleware(thunkMiddleware))
+  const wrapper = mount(<Provider store={store}><Router><SignUp /></Router></Provider>)
+  const form = wrapper.find(VanillaSignUp)
+  const input = wrapper.find(Form.Input).first()
+  const realInput = input.find('input')
+  realInput.instance().value = 'hi'
+  realInput.simulate('keyUp')
+  expect(form.instance().state.firstName).toBe('hi')
 })
