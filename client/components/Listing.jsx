79c621b9bc26fb3c044857375b64747fd04b9478
Lache Melvin <lache.melvin@gmail.com>
2020-05-11 12:44:43 +1200

    remove .then() after dispatches

--- client/components/Listing.jsx
+++ client/components/Listing.jsx
@@ -18,23 +18,19 @@ class Listing extends React.Component {
   }
   componentDidMount() {
     this.props.dispatch(getListingsPending())
-      .then(() => {
-        getListingById(this.props.match.params.id)
-          .then(listing => {
-            this.props.dispatch(getListingSuccess())
-              .then(() => {
-                if (listing === undefined) {
-                      this.props.history.push(`/404`)
-                } else {
-                  this.setState({
-                    listing,
-                    emailSubject: listing.name.split(' ').join('%20'),
-                    description: listing.description,
-                    imageUrl: listing.imageUrl
-                  })
-                }
-              })
+    getListingById(this.props.match.params.id)
+      .then(listing => {
+        this.props.dispatch(getListingSuccess())
+        if (listing === undefined) {
+              this.props.history.push(`/404`)
+        } else {
+          this.setState({
+            listing,
+            emailSubject: listing.name.split(' ').join('%20'),
+            description: listing.description,
+            imageUrl: listing.imageUrl
           })
+        }
       })
   }
 
