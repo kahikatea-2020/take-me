527aeaa772bcd4a494043f4a06713d8a524fc2ea
Lache Melvin <lache.melvin@gmail.com>
2020-05-11 15:30:12 +1200

    add category filtering to search dropdown

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -1,14 +1,18 @@
 import _ from 'lodash'
-import React, { Component, Link } from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Search } from 'semantic-ui-react'
+import { Search, Form } from 'semantic-ui-react'
 
-import { getListings } from '../actions/listings'
+import { getCategories } from '../actions/categories'
 
-const initialState = { isLoading: false, results: [], value: '' }
+const initialState = { isLoading: false, results: [], value: '', category: {} }
 
 class SearchBar extends Component {
   state = initialState
+
+  componentDidMount () {
+    this.props.dispatch(getCategories())
+  }
 
   handleResultSelect = (e, { result }) => this.props.history.push(`/listings/${result.id}`)
 
@@ -20,18 +24,29 @@ class SearchBar extends Component {
 
       const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
       const isMatch = (result) => re.test(result.title)
-      // if (this.props.selectedCategory !== []) {
-      //   const filteredResults = this.props.listings.filter(listing => listing.categoryId === this.props.selectedCategory.id)
-      //   this.setState({
-      //     isLoading: false,
-      //     results: _.filter(filteredResults, isMatch)
-      //   })
-      // }
+      let filteredResults = this.props.listings
+      if (this.state.category.id) {
+        if (this.state.category.id !== 100) {
+          filteredResults = this.props.listings.filter(listing => listing.categoryId === this.state.category.id)
+        }
+      }
       this.setState({
         isLoading: false,
-        results: _.filter(this.props.listings, isMatch)
-      })
+        results: _.filter(filteredResults, isMatch)
+      }) 
     }, 300)
+  }
+
+  handleCategoryChange = (e, data) => {
+    const id = data.options.find(category => data.value === category.value).key
+    const text = data.options.find(category => data.value === category.value).text
+    this.setState({
+      category: {
+        text: text,
+        id: id,
+        name: data.value
+      }
+    })
   }
 
   handleEnter = e => {
@@ -42,21 +57,36 @@ class SearchBar extends Component {
   }
 
   render () {
-    const { isLoading, value, results } = this.state   
+    const { isLoading, value, results } = this.state
+    const storeCategories = this.props.categories.map(category => ({
+      key: category.id,
+      text: category.name,
+      value: category.name
+    }))
+    const categories = [{ key: 100, text: 'All Categories', value: 'All Categories' }, ...storeCategories]
     return (
       <>
-          <Search
-            width={10}
-            loading={isLoading}
-            onResultSelect={this.handleResultSelect}
-            onSearchChange={_.debounce(this.handleSearchChange, 500, {
-              leading: true
-            })}
-            onKeyDown={this.handleEnter}
-            results={results}
-            value={value}
-            {...this.props}
-          />
+        <Form.Select
+          width={6}
+          onChange={this.handleCategoryChange}
+          fluid
+          options={categories}
+          placeholder='Category'
+          name='category'
+          value={this.state.category.name}
+        />
+        <Search
+          width={10}
+          loading={isLoading}
+          onResultSelect={this.handleResultSelect}
+          onSearchChange={_.debounce(this.handleSearchChange, 500, {
+            leading: true
+          })}
+          onKeyDown={this.handleEnter}
+          results={results}
+          value={value}
+          {...this.props}
+        />
       </>
     )
   }
@@ -66,11 +96,13 @@ const mapStateToProps = state => {
   const listings = state.listings.map(listing => ({
       title: listing.name,
       image: `https://res.cloudinary.com/takemenz/image/upload/${listing.imageUrl[0]}`,
-      id: listing.id
+      id: listing.id,
+      categoryId: listing.categoryId
     })
   )
   return {
-    listings: listings
+    listings: listings,
+    categories: state.categories
   }
 }
 
