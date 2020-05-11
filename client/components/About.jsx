6dad56ba6850ae31ab36132bba1799615e32671e
Mathias Bast <mathiasbast123@gmail.com>
2020-05-11 21:48:00 +1200

    more set up

---
+++ client/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class About extends React.Component {
+  render () {
+    return (
+      <h1>Welcome To Take Me</h1>
+      <h2>What is The Point of This Website?</h2>
+      <p></p>
+    )
+  }
+}
+
+export default About
