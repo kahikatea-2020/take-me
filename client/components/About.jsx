c12eecd31d1569a11a9978a49efa0c1c7aa8a69c
Mathias Bast <mathiasbast123@gmail.com>
2020-05-11 22:25:10 +1200

    adds a paragraph

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -6,6 +6,10 @@ class About extends React.Component {
       <>
         <h1>Welcome To Take Me</h1>
         <h2>What is The Point of This Website?</h2>
+        <p>
+        We want to promote recycling and reusing things that do not need to be thrown out. "one man's trash is another man's treasure"
+        Is how our site works. We aim to make it easy to find someone else who wants something you will throw out and give it a second life.
+        </p>
 
       </>
     )
