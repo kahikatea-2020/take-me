26ebf73a024e4426bb828f9309c99fbdaa6467d9
Lache Melvin <lache.melvin@gmail.com>
2020-05-12 11:22:14 +1200

    remove unnecessary imports

--- client/actions/q-and-a.js
+++ client/actions/q-and-a.js
@@ -1,6 +1,3 @@
-import * as api from '../api/q-and-a'
-import { showError } from './error'
-
 export const GET_COMMENTS_PENDING = 'GET_COMMENTS_PENDING'
 export const GET_COMMENTS_SUCCESS = 'GET_COMMENTS_SUCCESS'
 
