dba5f8d18fbe488c1769930cdd53454bd5bedda5
Mathias Bast <mathiasbast123@gmail.com>
2020-05-13 12:31:44 +1200

    working

--- client/components/Error404.jsx
+++ client/components/Error404.jsx
@@ -5,14 +5,19 @@ import { Button } from 'semantic-ui-react'
 class Error404 extends React.Component {
   render () {
     return (
-      <div className="ui placeholder segment">
-        <div className="ui icon header">
-          <i className="search icon"></i>
-          <h1>404</h1>
-        </div>
-        <div className="inline">
-          <div className="ui primary button">Clear Query</div>
-          <div className="ui button">Add Document</div>
+      <div className='wrapper'>
+        <div className="ui placeholder segment">
+          <div className="ui icon header">
+            <i className="search icon"></i>
+            <h1 style={{ color: 'red', fontSize: '40px' }}> 404 </h1>
+            <h2>Sorry, we can't find the page you requested.</h2>
+            <h3>You may have clicked on a broken link or entered an incorrect URL.</h3>
+          </div>
+          <br />
+          <div className="inline">
+            <Button id='home' as={Link} to='/' >Home</Button>
+            <Button id='home' as='a' onClick={() => this.props.history.goBack()} to='/' >Back</Button>
+          </div>
         </div>
       </div>
     )
