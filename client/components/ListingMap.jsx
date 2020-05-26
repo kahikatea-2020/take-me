23793cf3b4d7738954638932fbdc7fca0604737e
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-26 16:30:30 +1200

    Add API key

--- client/components/ListingMap.jsx
+++ client/components/ListingMap.jsx
@@ -5,7 +5,7 @@ import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps'
 function MapLocation () {
   return (
     <GoogleMap
-      defaultZoom={10}
+      defaultZoom={16}
       defaultCenter={{ lat: -36.864480, lng: 174.776730 }}
     />
   )
@@ -17,7 +17,7 @@ export default function ListingMap () {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <WrappedMap
-        googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'}
+        googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBo31yLsQW_1lYvSa4rYMqVkGyF1H7KDBo'}
         loadingElement={<div style={{ height: '100%' }}/>}
         containerElement={<div style={{ height: '100%' }}/>}
         mapElement={<div style={{ height: '100%' }}/>}
