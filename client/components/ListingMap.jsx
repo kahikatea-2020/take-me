c64337dad976f6695e98234dffbd7fd655db52b6
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-27 14:25:22 +1200

    Implement Geocoder to get coordinates from suburb & city

--- client/components/ListingMap.jsx
+++ client/components/ListingMap.jsx
@@ -1,25 +1,46 @@
 import React from 'react'
+import Geocode from 'react-geocode'
 
 import { GoogleMap, withScriptjs, withGoogleMap, Circle } from 'react-google-maps'
 
-function MapLocation () {
-  return (
-    <GoogleMap
-      defaultZoom={14}
-      defaultCenter={{ lat: -36.864480, lng: 174.776730 }}
-    >
-      <Circle
-        center={{ lat: -36.864480, lng: 174.776730 }}
-        radius={800} /* radius is set in metres */
-        options={{
-          strokeColor: '#f79421',
-          strokeOpacity: 0.95,
-          fillColor: '#f79421',
-          fillOpacity: 0.35
-        }}
-      />
-    </GoogleMap>
-  )
+Geocode.setApiKey('AIzaSyBo31yLsQW_1lYvSa4rYMqVkGyF1H7KDBo')
+
+class MapLocation extends React.Component {
+  state = {
+    lat: null,
+    lng: null
+  }
+
+  render () {
+    Geocode.fromAddress('Ellerslie, Auckland').then(
+      res => {
+        const { lat, lng } = res.results[0].geometry.location
+        this.setState({ lat, lng })
+      },
+      error => {
+        console.log(error)
+      }
+    )
+    return (
+      <>
+        <GoogleMap
+          zoom={14}
+          center={{ lat: this.state.lat, lng: this.state.lng }}
+        >
+          <Circle
+            center={{ lat: this.state.lat, lng: this.state.lng }}
+            radius={800} /* radius is set in metres */
+            options={{
+              strokeColor: '#f79421',
+              strokeOpacity: 0.95,
+              fillColor: '#f79421',
+              fillOpacity: 0.35
+            }}
+          />
+        </GoogleMap>
+      </>
+    )
+  }
 }
 
 const WrappedMap = withScriptjs(withGoogleMap(MapLocation))
