4bbf0376da42121b26739344b21c553102106cc3
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-28 11:56:06 +1200

    Change to a stateful component

--- client/components/ListingMap.jsx
+++ client/components/ListingMap.jsx
@@ -1,33 +1,38 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactMapGL, { Marker } from 'react-map-gl'
 
-function ListingMap () {
-  const [viewport, setViewport] = useState({
-    latitude: -36.864479,
-    longitude: 174.776733,
-    width: '30vw',
-    height: '30vh',
-    zoom: 12
-  })
-  return (
-    <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      mapStyle='mapbox://styles/ellorav/ckapzm89l24ph1jlpo6wdzq1q'
-      onViewportChange={viewport => {
-        setViewport(viewport)
-      }}
-    >
-      <Marker
-        latitude={-36.864479}
-        longitude={174.776733}
+class ListingMap extends React.Component {
+  state = {
+    viewport: {
+      latitude: -36.864479,
+      longitude: 174.776733,
+      width: '30vw',
+      height: '30vh',
+      zoom: 12
+    }
+  }
+
+  render () {
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle='mapbox://styles/ellorav/ckapzm89l24ph1jlpo6wdzq1q'
+        onViewportChange={viewport => {
+          this.setState(viewport)
+        }}
       >
-        <button className='marker-btn'>
-          <img src='/favicon-take-me.ico' alt='TakeMe icon as item marker' />
-        </button>
-      </Marker>
-    </ReactMapGL>
-  )
+        <Marker
+          latitude={-36.864479}
+          longitude={174.776733}
+        >
+          <button className='marker-btn'>
+            <img src='/favicon-take-me.ico' alt='TakeMe icon as item marker' />
+          </button>
+        </Marker>
+      </ReactMapGL>
+    )
+  }
 }
 
 export default ListingMap
