cf0b9e480765f436ad802564a14f098d16b2735d
Ellora Virtue <ellora.virtue@hotmail.com>
2020-05-31 16:25:02 +1200

    Fix quotation marks to make code style consistent

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -10,15 +10,15 @@ class About extends React.Component {
   render () {
     return (
       <>
-        <div className="aboutHero">
-          {/* <div className="aboutLogo">
+        <div className='aboutHero'>
+          {/* <div className='aboutLogo'>
             <h1>Welcome to</h1>
-            <img src='about logo-05.png' alt=""/>
+            <img src='about logo-05.png' alt=''/>
           </div> */}
 
-          <div className="aboutHeroText">
+          <div className='aboutHeroText'>
             <div style={{ textAlign: 'right', paddingRight: '4%' }}>
-              <p id="welcome">Welcome to <span id='takeme2nd'> take me</span></p>
+              <p id='welcome'>Welcome to <span id='takeme2nd'> take me</span></p>
 
             </div>
             <h3>
@@ -29,55 +29,55 @@ class About extends React.Component {
             </p>
           </div>
         </div>
-        <div className="aboutSections" data-aos='fade-up'>
+        <div className='aboutSections' data-aos='fade-up'>
           <h2>Reduce, reuse &amp; recycle</h2>
           <p>
           This is a concept our team is promoting through creating this website. By choosing our website, and in turn choosing a pathway to sustainable living, you are also choosing to make the following positive impacts:
           </p>
           <List
-            id="aboutList"
+            id='aboutList'
             data-aos='fade-left'
             data-aos-duration='1000'
             data-aos-offset='250'
           >
-            <List.Item id="aboutListItem">
-              <List.Header id="aboutSecHeaders">Energy conservation</List.Header>
+            <List.Item id='aboutListItem'>
+              <List.Header id='aboutSecHeaders'>Energy conservation</List.Header>
               Using our website reduces product demand of manufacturers, thus saving energy.
             </List.Item>
-            <List.Item id="aboutListItem">
-              <List.Header id="aboutSecHeaders">Reduces landfill</List.Header>
+            <List.Item id='aboutListItem'>
+              <List.Header id='aboutSecHeaders'>Reduces landfill</List.Header>
               By re-purposing and/or recycling materials and products, we reduce the amount of waste that ends up in
               landfills, which altogether contributes to a healthier planet.
             </List.Item>
-            <List.Item id="aboutListItem">
-              <List.Header id="aboutSecHeaders">Less toxic chemicals</List.Header>
+            <List.Item id='aboutListItem'>
+              <List.Header id='aboutSecHeaders'>Less toxic chemicals</List.Header>
               Factories that produce the products we see on shelves are often made from raw materials which create toxic chemicals. Re-purposing is a simple solution to lowering the demand, and therefore the production of these toxins.
             </List.Item>
-            <List.Item id="aboutListItem">
-              <List.Header id="aboutSecHeaders">Reduces pollution</List.Header>
+            <List.Item id='aboutListItem'>
+              <List.Header id='aboutSecHeaders'>Reduces pollution</List.Header>
               A lot of our so called rubbish can actually be recycled. Opting for this method can reduce the amount of waste that would otherwise end up in our oceans.
             </List.Item>
-            <List.Item id="aboutListItem">
-              <List.Header id="aboutSecHeaders">Saves you money</List.Header>
+            <List.Item id='aboutListItem'>
+              <List.Header id='aboutSecHeaders'>Saves you money</List.Header>
               If you are getting something free from our website, you are not buying something new. As a result, you are saving money!
             </List.Item>
           </List>
         </div>
         <div
-          className="aboutSections"
-          id="photoCard"
+          className='aboutSections'
+          id='photoCard'
           data-aos='zoom-in-up'
           data-aos-offset='200'
           data-aos-duration='1500'>
-          <div id="testDivo">
-            <img src="https://images.unsplash.com/photo-1568085823039-e823e87197e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt=""/>
-            <div id="aboutCardHeader">
+          <div id='testDivo'>
+            <img src='https://images.unsplash.com/photo-1568085823039-e823e87197e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' alt=''/>
+            <div id='aboutCardHeader'>
               <h3>If you want to clean out your closet, garage or clutter, why waste it when you can give it away?</h3>
             </div>
           </div>
         </div>
         <div
-          className="aboutSections"
+          className='aboutSections'
           data-aos='fade-up'
           data-aos-offset='200'
           data-aos-duration='700'
@@ -95,68 +95,68 @@ class About extends React.Component {
           responsibility to look after our planet. This site can help you reduce how much rubbish you produce and make someone else happy by doing so!
           </p>
         </div>
-        <h4 id="thanks">Thank you!</h4>
+        <h4 id='thanks'>Thank you!</h4>
         <div
-          className="aboutSections"
-          id="teamWrapper"
+          className='aboutSections'
+          id='teamWrapper'
           data-aos='zoom-in'
           data-aos-offset='250'
           data-aos-duration='1000'
         >
-          <div className="teamPics">
-            <div className="takemeTeam">
+          <div className='teamPics'>
+            <div className='takemeTeam'>
               <Image src='./team/lache.png' size='medium' circular />
-              <Header as='h2' attached='top' id="teamSegment">
+              <Header as='h2' attached='top' id='teamSegment'>
               Lache Melvin
               </Header>
-              <Segment attached id="teamSegment">
+              <Segment attached id='teamSegment'>
                 <p>Vibes lead</p>
               </Segment>
             </div>
-            <div className="takemeTeam">
+            <div className='takemeTeam'>
               <Image src='./team/ellora.jpg' size='medium' circular />
-              <Header as='h2' attached='top' id="teamSegment">
+              <Header as='h2' attached='top' id='teamSegment'>
               Ellora Virtue
               </Header>
-              <Segment attached id="teamSegment">
+              <Segment attached id='teamSegment'>
                 <p>Scrum Master</p>
               </Segment>
             </div>
-            <div className="takemeTeam">
+            <div className='takemeTeam'>
               <Image src='./team/john.jpg' size='medium' circular />
-              <Header as='h2' attached='top' id="teamSegment">
+              <Header as='h2' attached='top' id='teamSegment'>
             John Sengson
               </Header>
-              <Segment attached id="teamSegment">
+              <Segment attached id='teamSegment'>
                 <p>Product Owner</p>
               </Segment>
             </div>
           </div>
-          <div className="teamPics">
-            <div className="takemeTeam">
+          <div className='teamPics'>
+            <div className='takemeTeam'>
               <Image src='./team/pat.jpg' size='medium' circular />
-              <Header as='h2' attached='top' id="teamSegment">
+              <Header as='h2' attached='top' id='teamSegment'>
               Pat Lim
               </Header>
-              <Segment attached id="teamSegment">
+              <Segment attached id='teamSegment'>
                 <p>Back-end Lead</p>
               </Segment>
             </div>
-            <div className="takemeTeam">
+            <div className='takemeTeam'>
               <Image src='./team/hamish.jpg' size='medium' circular />
-              <Header as='h2' attached='top' id="teamSegment">
+              <Header as='h2' attached='top' id='teamSegment'>
              Hamish Henare
               </Header>
-              <Segment attached id="teamSegment">
+              <Segment attached id='teamSegment'>
                 <p>Front-end Lead</p>
               </Segment>
             </div>
-            <div className="takemeTeam">
+            <div className='takemeTeam'>
               <Image src='./team/mathias.jpg' size='medium' circular />
-              <Header as='h2' attached='top' id="teamSegment">
+              <Header as='h2' attached='top' id='teamSegment'>
               Mathias Bast
               </Header>
-              <Segment attached id="teamSegment">
+              <Segment attached id='teamSegment'>
                 <p>Git Lead</p>
               </Segment>
             </div>
